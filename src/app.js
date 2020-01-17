5adacf36c09f07fd46eef474987e424af7130aa2
Patrick Gustafson <patrickgus@charter.net>
2020-01-16 21:53:37 -0800

    import errorHandler validation and foldersRouter

--- src/app.js
+++ src/app.js
@@ -4,28 +4,25 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const validateBearerToken = require("./validate-bearer-token");
+const errorHandler = require("./error-handler");
+const foldersRouter = require("./folders/folders-router");
+// const notesRouter = require("./notes/notes-router");
 
 const app = express();
 
-const morganOption = NODE_ENV === "production" ? "tiny" : "common";
-
-app.use(morgan(morganOption));
+app.use(
+  morgan(NODE_ENV === "production" ? "tiny" : "common", {
+    skip: () => NODE_ENV === "test"
+  })
+);
 app.use(helmet());
 app.use(cors());
+app.use(validateBearerToken);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.use("/api/folders", foldersRouter);
+// app.use("/api/notes", notesRouter);
 
-app.use((error, req, res, next) => {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
