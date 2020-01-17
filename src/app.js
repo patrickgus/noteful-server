4029c1643995907617742be9048e17d5469a1453
Patrick Gustafson <patrickgus@charter.net>
2020-01-17 08:07:42 -0800

    delete logger and token validation

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-const validateBearerToken = require("./validate-bearer-token");
 const errorHandler = require("./error-handler");
 const foldersRouter = require("./folders/folders-router");
 // const notesRouter = require("./notes/notes-router");
@@ -18,7 +17,6 @@ app.use(
 );
 app.use(helmet());
 app.use(cors());
-app.use(validateBearerToken);
 
 app.use("/api/folders", foldersRouter);
 // app.use("/api/notes", notesRouter);
